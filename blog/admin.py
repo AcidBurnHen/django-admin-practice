a5c9441b94b4e38ce1116d97f088b68b8f2676c2
acidburn <cc.acidburn@gmail.com>
2023-02-24 20:50:28 +0100

    custom admin route

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models 
 
-# Register your models here.
+class BlogAdminArea(admin.AdminSite):
+    site_header = 'Blog Admin Area'
+
+blog_site = BlogAdminArea(name='BlogAdmin')
+
+blog_site.register(models.Post)
